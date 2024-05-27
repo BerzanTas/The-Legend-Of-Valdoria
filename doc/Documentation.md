bdf9dd2a6c4ce39b859deea7d99c93030f3470c1
Berzan Tas <tberzan@gmail.com>
2024-05-27 09:00:25 +0200

    repo structure changed, intro to the game added

---
+++ doc/Documentation.md
@@ -0,0 +1,11 @@
+# Documentation
+
+Hi! This file is for our future selves. Here I will write down the pygame functions used in our project and what they do.
+
+
+ ## pygame.display.get_surface()
+
+Using `pygame.display.get_surface()` is particularly useful in more complex projects where multiple modules or functions may need access to the main display surface without having to pass it as an argument each time.
+
+In summary, `pygame.display.get_surface()` provides a convenient way to **obtain a reference to the main surface on which all game elements are drawn**. This allows for a more flexible and modular approach to programming in Pygame.
+
---
+++ src/main.py
@@ -0,0 +1,51 @@
+import pygame, sys
+from level import Level
+from settings import *
+from pyvidplayer import Video
+
+class Game:
+    def __init__(self) -> None:
+        # setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
+        self.clock = pygame.time.Clock()
+        self.level = Level()
+        # changing the title of the window
+        pygame.display.set_caption("The Legend of Valdoria")
+        self.vid = Video("Intro/Valdoria Intro.mp4")
+        self.vid.set_size((WIDTH, HEIGTH))
+    
+    def intro(self):
+        while self.vid.active:
+            self.vid.draw(self.screen, (0,0), False)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.vid.close()
+                        self.run()
+                elif event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+        
+        self.run()
+
+    # main loop
+    def run(self):
+        while True:
+            # check the events, and if it is QUIT event then close the game
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            self.screen.fill('black')
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.intro()
