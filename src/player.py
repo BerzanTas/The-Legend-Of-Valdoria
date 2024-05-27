03cfa0acdaf28b4528b19fc23237f5844bcf9aa2
Berzan Tas <tberzan@gmail.com>
2024-05-27 10:07:42 +0200

    added player sprite sheet, little changes to code

---
+++ src/player.py
@@ -0,0 +1,8 @@
+import pygame
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups) -> None:
+        super().__init__(groups)
+        self.image = pygame.image.load("img/playersprite.png").convert_alpha()
+        self.rect = self.image.get_rect(topleft = pos)
--- src/tile.py
+++ src/tile.py
@@ -4,4 +4,6 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
+        self.image = pygame.image.load("img/rock.png").convert_alpha()
+        self.rect = self.image.get_rect(topleft = pos)
         
