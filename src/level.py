110960e7941ccc5240e9c9c0c3b09b20944cf676
Berzan Tas <tberzan@gmail.com>
2024-06-10 14:49:33 +0200

    Ysort added

--- src/level.py
+++ src/level.py
@@ -6,27 +6,7 @@ from slime import Slime
 from projectile import Fireball
 from ui import UI
 from skeleton import Skeleton
-
-class Camera:
-    def __init__(self, width, height):
-        self.camera = pygame.Rect(0, 0, width, height)
-        self.width = width
-        self.height = height
-
-    def apply(self, entity):
-        return entity.rect.move(self.camera.topleft)
-
-    def update(self, target):
-        x = -target.rect.centerx + int(WIDTH / 2)
-        y = -target.rect.centery + int(HEIGTH / 2)
-
-        # Limit scroll to map size
-        x = min(0, x)  # Left
-        y = min(0, y)  # Top
-        x = max(-(self.width - WIDTH), x)  # Right
-        y = max(-(self.height - HEIGTH), y)  # Bottom
-
-        self.camera = pygame.Rect(x, y, self.width, self.height)
+from elements import *
 
 class Level:
     def __init__(self):
@@ -55,22 +35,17 @@ class Level:
                     self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites, self.fireball_sprites, self.visible_sprites)
                 elif col == 'x':
                     Tile((x, y), (self.visible_sprites, self.obstacle_sprites), 'rock')
-                elif col == 'c':
-                    castle_pos = (x, y)
-                    self.castle = Castle(castle_pos, (self.visible_sprites, self.obstacle_sprites))
-                    #self.visible_sprites.change_layer(self.castle, 2)
                 elif col == 's':
                     self.slime = Slime((x, y), (self.visible_sprites, self.obstacle_sprites), self.obstacle_sprites, self.visible_sprites, self.player)
-                    #self.visible_sprites.change_layer(self.slime, 1)
                 elif col == 'sk':
                     self.slime = Skeleton((x, y), (self.visible_sprites, self.obstacle_sprites), self.obstacle_sprites, self.visible_sprites, self.player)
-                    #self.visible_sprites.change_layer(self.slime, 1)
                 elif col == "tr1": #drzewo 1
                     Tile((x, y), (self.visible_sprites, self.obstacle_sprites), 'tree1', layer=2)
                 elif col == "portal":  # portal
                     Portal((x, y), (self.visible_sprites,))
                 elif col == "w":  #woda
                     Water((x, y), (self.visible_sprites, self.obstacle_sprites))
+                    
                 
     def run(self):
 
@@ -97,7 +72,8 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
 
-        for sprite in self.sprites():
+        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, sprite.rect)
+            pygame.draw.rect(self.display_surface, "red", sprite.hitbox, 3)
     
